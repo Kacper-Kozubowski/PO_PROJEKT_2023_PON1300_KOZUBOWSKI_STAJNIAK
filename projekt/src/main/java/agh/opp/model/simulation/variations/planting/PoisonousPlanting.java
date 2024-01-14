e955dae6f7be7cfb1da2c65154d905bcdf6615e5
zuniak <zstajniak@student.agh.edu.pl>
2024-01-14 02:12:46 +0100

    Planting Resolves #11

---
+++ projekt/src/main/java/agh/opp/model/simulation/variations/planting/PoisonousPlanting.java
@@ -0,0 +1,65 @@
+package agh.opp.model.simulation.variations.planting;
+
+import agh.opp.model.elements.Plant;
+import agh.opp.model.elements.PoisonousPlant;
+import agh.opp.model.tools.Randomizer;
+import agh.opp.model.tools.Vector2d;
+import agh.opp.model.tools.interfaces.WorldMap;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PoisonousPlanting extends AbstractPlanting {
+    private final Randomizer randomizer = new Randomizer();
+    private final Set<Vector2d> poisonousArea = new HashSet<>();
+    private final Set<Vector2d> normalArea;
+
+    public PoisonousPlanting(WorldMap map, int height, int width, int startPlant, int energy) {
+        super(map, energy);
+        normalArea = new HashSet<>(map.getArea());
+        getPoisonousArea(height, width);
+        plant(startPlant);
+    }
+
+    @Override
+    public void plant(int toPlant) {
+        Set<Vector2d> taken = map.getPlants().keySet();
+        List<Vector2d> free = normalArea.stream().filter(i -> !taken.contains(i)).collect(Collectors.toList());
+        Collections.shuffle(free);
+
+
+        for (int planted = 0; planted < toPlant; planted++) {
+            boolean mapFull = (free.size() - planted) <= 0;
+            if (!mapFull) {
+                Vector2d position = free.get(planted);
+                if (poisonousArea.contains(position) && randomizer.coinToss()) {
+                    Plant plant = new PoisonousPlant(position, plantEnergy);
+                    map.addPlant(plant);
+                } else {
+                    Plant plant = new Plant(position, plantEnergy);
+                    map.addPlant(plant);
+                }
+            } else {
+                System.err.println("Map is full");
+                return;
+            }
+        }
+    }
+
+    private void getPoisonousArea(int height, int width) {
+        int size = (height * width) / 5;
+        int a = (int) Math.sqrt(size);
+
+        Random random = new Random();
+        int x1 = random.nextInt(height - a + 1);
+        int y1 = random.nextInt(width - a + 1);
+
+        for (int i = 0; i < a; i++) {
+            int x = x1 + i;
+            for (int j = 0; j < a; j++) {
+                int y = y1 + j;
+                poisonousArea.add(new Vector2d(x, y));
+            }
+        }
+    }
+}
---
+++ projekt/src/main/java/agh/opp/model/simulation/variations/planting/RegularPlanting.java
@@ -0,0 +1,93 @@
+package agh.opp.model.simulation.variations.planting;
+
+import agh.opp.model.tools.Randomizer;
+import agh.opp.model.tools.Vector2d;
+import agh.opp.model.tools.interfaces.WorldMap;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RegularPlanting extends AbstractPlanting {
+    private final Randomizer random = new Randomizer();
+    private final Set<Vector2d> equatorArea = new HashSet<>();
+    private final Set<Vector2d> normalArea;
+
+
+    public RegularPlanting(WorldMap map, int height, int width, int startPlant, int plantEnergy) {
+        super(map, plantEnergy);
+        generateEquatorArea(height, width);
+        normalArea = new HashSet<>(map.getArea());
+        normalArea.removeAll(equatorArea);
+        plant(startPlant);
+    }
+
+    @Override
+    public void plant(int toPlant) {
+        Set<Vector2d> taken = map.getPlants().keySet();
+        List<Vector2d> freeEquator = equatorArea.stream().filter(i -> !taken.contains(i)).collect(Collectors.toList());
+        List<Vector2d> freeNormal = normalArea.stream().filter(i -> !taken.contains(i)).collect(Collectors.toList());
+        Collections.shuffle(freeEquator);
+        Collections.shuffle(freeNormal);
+
+        int toEquator = 0;
+        int toNormal = 0;
+
+        for (int i = 0; i < toPlant; i++) {
+            boolean equatorFull = (freeEquator.size() - toEquator) <= 0;
+            boolean normalFull = (freeNormal.size() - toNormal) <= 0;
+
+            if (!equatorFull && !normalFull) {
+                if (pareto()) {
+                    addPlant(freeEquator.get(toEquator));
+                    toEquator++;
+                } else {
+                    addPlant(freeNormal.get(toNormal));
+                    toNormal++;
+                }
+            } else if (!equatorFull) {
+                addPlant(freeEquator.get(toEquator));
+                toEquator++;
+            } else if (!normalFull) {
+                addPlant(freeNormal.get(toNormal));
+                toNormal++;
+            } else {
+                System.err.println("Map is full");
+                return;
+            }
+        }
+    }
+
+    private boolean pareto() {return random.randomize(80);}
+
+    private void generateEquatorArea(int height, int width) {
+        int size = (height * width) / 5;
+        int mid1, mid2;
+
+        if (height % 2 == 0) {
+            mid1 = height / 2;
+            mid2 = mid1 - 1;
+        } else {
+            mid1 = mid2 = height / 2;
+        }
+
+        while (size > 0) {
+            Set<Vector2d> equator = new HashSet<>();
+            for (int x = 0; x < width; x++) {
+                equator.add(new Vector2d(x, mid1));
+                equator.add(new Vector2d(x, mid2));
+            }
+
+            if (equator.size() <= size) {
+                equatorArea.addAll(equator);
+                mid1++;
+                mid2--;
+                size -= equator.size();
+            } else {
+                List<Vector2d> random = new ArrayList<>(equator);
+                Collections.shuffle(random);
+                equatorArea.addAll(random.subList(0, size));
+                return;
+            }
+        }
+    }
+}
